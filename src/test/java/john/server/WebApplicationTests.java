8f72c9ca1706aa671a2143da200b5aa3a38b1e00
Jhon Andrie <jhonandriecanedo1@gmail.com>
2023-11-28 14:51:45 +0800

    Master Branch

---
+++ src/test/java/john/server/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package john.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
